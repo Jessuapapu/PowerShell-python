a27a6fa62de2f1231a6c71ec03f131a0840c0a6f
Jessua Rene Solis Juarez <jessuarene@gmail.com>
2026-09-03 20:58:37 -0600

    feat(ls)

---
+++ Main.py
@@ -0,0 +1,13 @@
+import subprocess
+from ls.Main_ls import *
+
+def main():
+# Comando que deseas ejecutar en PowerShell
+    path = 'C:/'
+    while True:
+
+        comando = input("Ingrese el comando de PowerShell que desea ejecutar: ")
+        
+
+if "__main__":
+    main()
